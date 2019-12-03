9f6fbddf9048b537f3855fec3bedcf5acb36d76a
Kevin Coakley <kcoakley@sdsc.edu>
2019-12-02 16:38:57 -0800

    Log to error instead of info when there is an IOError

--- twitterstreamarchive/file_writer.py
+++ twitterstreamarchive/file_writer.py
@@ -11,5 +11,5 @@ def write_gzip(archive_path, status):
                        "at", encoding='utf8') as f:
             f.write(str(status))
     except IOError as ex:
-        logging.info("Error writing to archive: %s/%s: %s", archive_path,
-                     datetime.datetime.now().strftime("%Y%m%d-%H-tweets.txt.gz"), ex)
+        logging.error("Error writing to archive: %s/%s: %s", archive_path,
+                      datetime.datetime.now().strftime("%Y%m%d-%H-tweets.txt.gz"), ex)
