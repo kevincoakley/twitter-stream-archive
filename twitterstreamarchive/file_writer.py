7577777395973825c819087d1de4a639f3a47a0f
Kevin Coakley <kcoakley@sdsc.edu>
2020-03-05 09:31:17 -0800

    Fix for end of line issues with saved files

--- twitterstreamarchive/file_writer.py
+++ twitterstreamarchive/file_writer.py
@@ -9,7 +9,7 @@ def write_gzip(archive_path, status):
     try:
         with gzip.open(archive_path + "/" + datetime.datetime.now().strftime("%Y%m%d-%H-tweets.txt.gz"),
                        "at", encoding='utf8') as f:
-            f.write(str(status))
+            f.write(str(status).strip() + "\n")
     except IOError as ex:
         logging.error("Error writing to archive: %s/%s: %s", archive_path,
                       datetime.datetime.now().strftime("%Y%m%d-%H-tweets.txt.gz"), ex)
