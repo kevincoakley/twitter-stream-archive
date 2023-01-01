3187c7125fbdb1e8683cf6bc73ade76d09fa979d
Kevin Coakley <kcoakley@sdsc.edu>
2022-12-31 19:49:23 -0800

    Add support to change the datetime for the v2 Twitter api
    Signed-off-by: Kevin Coakley <kcoakley@sdsc.edu>

--- twitterstreamarchive/transform_tweet.py
+++ twitterstreamarchive/transform_tweet.py
@@ -15,13 +15,30 @@ def convert_created_at(status):
     """
     status_json = json.loads(status)
 
-    # Convert "Wed Nov 27 23:27:39 +0000 2019" to "2019-11-27 23:27:39"
+    # For the v1.1 Twitter API 
     if "created_at" in status_json:
+        #
+        # Convert "Wed Nov 27 23:27:39 +0000 2019" to "2019-11-27 23:27:39"
+        #
         created_at = status_json["created_at"]
         # Convert created_at to a datetime object
         created_at_in_datetime = datetime.datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")
         # Convert created_at_in_datetime object to the new format
         created_at_converted = created_at_in_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        # Add the created_at_converted element to the json
+        status_json["created_at_converted"] = created_at_converted
+    # For the v2 Twitter API
+    elif "created_at" in status_json["data"]:
+        #
+        # Convert "2023-01-01T03:04:27.000Z" to "2023-01-01 03:04:27"
+        #
+        created_at = status_json["data"]["created_at"]
+        # Convert created_at to a datetime object
+        created_at_in_datetime = datetime.datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%S.000Z")
+        # Convert created_at_in_datetime object to the new format
+        created_at_converted = created_at_in_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        # Add the created_at_converted element to the json
+        status_json["data"]["created_at_converted"] = created_at_converted
     else:
         # If status is a delete or a status_withheld status then just return status
         if "delete" in status_json or "status_withheld" in status_json:
@@ -29,8 +46,5 @@ def convert_created_at(status):
         # If status is not a delete status then there is probably an error
         logger.info("Tweet does not have a created_at variable: %s", status)
         return status
-
-    # Add the created_at_converted element to the json
-    status_json["created_at_converted"] = created_at_converted
 
     return json.dumps(status_json, ensure_ascii=False)
